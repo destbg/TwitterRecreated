241383e9ea0b3904a874f61b0a654b39e2e00806
Nikolai <35627380+destbg@users.noreply.github.com>
2020-02-08 12:02:52 +0200

    Update CurrectUserService.cs

--- src/WebApi/Services/CurrectUserService.cs
+++ src/WebApi/Services/CurrectUserService.cs
@@ -2,25 +2,32 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Application.Common.Interfaces;
-using Common;
 using Domain.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace WebApi.Services
 {
     public class CurrentUserService : ICurrentUserService
     {
+        private readonly ILogger<CurrentUserService> _logger;
+
+        public CurrentUserService(ILogger<CurrentUserService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public async Task Initialize(ClaimsPrincipal user, ConnectionInfo connection, IUserManager userManager)
         {
             if (user?.Identity?.IsAuthenticated == true)
             {
-
                 User = await userManager.GetCurrentUser(user.Identity.Name)
                     ?? throw new UnauthorizedAccessException("Provided credentials are invalid");
 
                 IsAuthenticated = true;
             }
             Ip = connection?.RemoteIpAddress?.ToString();
+            _logger.LogInformation(Ip);
         }
 
         public string Ip { get; private set; }
